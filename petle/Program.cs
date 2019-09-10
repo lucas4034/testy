d5edc428d4be61e8c0eb926bf410890523f2b1aa
USER <lukasz18@gmail.com>
2019-09-10 14:00:11 +0200

    Initial Commit

---
+++ petle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace petle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*
+             *  pętle:
+             *  for (int i=0; i<10;i++ )
+             *      
+             *      
+             *      
+             */
+             for (int i=0;i<4;i++) //zmienna i istnieje tylko wewnątrz pętli;
+            {
+                Console.WriteLine("HELLo " + i);
+
+            }
+
+            int b = 1;
+
+
+             for (;b<10;b++) //mozna pomijac elementy np for ( ; ;) <- petla nieskonczona
+            {
+                Console.WriteLine("Jedziemy z b " + b);
+            }
+
+            Console.WriteLine("Hello World!");
+
+            // Console.WriteLine(i);
+            Console.ReadLine();
+
+        }
+    }
+}
